e456b2cec5da427656f573881ac6a5e596eeba9b
Tim Tucker <tntucker0015@outlook.com>
2018-07-01 14:45:55 -0400

    adding server.js

---
+++ server.js
@@ -0,0 +1,64 @@
+var express =require("express");
+var bodyParser = require("body-parser");
+var logger = require('morgan');
+var mongoose = require("mongoose");
+var axios =require("axios");
+var cheerio = require("cheerio");
+var db = require("./models");
+var PORT = 3000;
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended:true}));
+app.use(express.static('public'));
+
+mongoose.connect('mongodb/localhost/pokemonNews');
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.pokemon.com/us/pokemon-news/").then(function(response) {
+    var $ =cheerio.load(response.data);
+    $("article h3").each(function(i, element) {
+      var result ={};
+
+      result.title =$(this)
+        .children("a")
+        .text();
+      result.lin = $(this)
+        .children("a")
+        .attr("href");
+
+      db.Article.create(result)
+      .then(function(dbArticle) {
+        console.log(dbArticle);
+      })
+      .catch(function(err) {
+        return res.json(err);
+      });
+    });
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+  .then(function(dbArticle) {
+    res,json(dbArticle);
+  })
+  .catch(function(err) {
+    res.json(err);
+  });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+  .then(function(dbNote) {
+    return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new:true });
+  })
+  .catch(function(err) {
+    res.json(err);
+  });
+});
+
+app.listen(PORT, function() {
+  console.log("app running on PROT " + PORT + "!");
+});
