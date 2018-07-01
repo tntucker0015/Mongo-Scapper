27a362275453079462a87295b789c22c9cd2b0be
Tim Tucker <tntucker0015@outlook.com>
2018-07-01 15:35:04 -0400

    adding public app.js

---
+++ public/app.js
@@ -0,0 +1,43 @@
+$.getJSON("/articles", function(data) {
+  for (var i = 0; i <data.length; i++) {
+    $("#articles").append("<p data-id='"data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+  }
+});
+
+$(document.on)("click", "p", function() {
+  $("#notes").empty();
+  var thisID = $(this.attr("data-id");
+$.ajax({
+  method: "get",
+  url: "/articles/" + thisId
+})
+.then(function(data) {
+  console.log(data);
+  $("#notes").append("<h3>" + data.title + "</h3>");
+  $("#notes").append("<input id='titleinput' name='title' >");
+  $("notes").append("<textarea id='bodyinput' name='body'></textarea>");
+  $("#notes").append("<button data-id='" + data_id + "'id='savenote'>Save Note</button>");
+  if (data.not){
+    $("#titleinput").val(data.note.title);
+    $("#bodyinput").val(data.note.body);
+  }
+});
+});
+
+$(document).on("click", "#savenote", function() {
+  var thisId = $(this).attr("data-id");
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data:{
+      title: $("#titlesinput").val(),
+      body: $("#bodyinput".val()
+    }
+  })
+  .then(function(data) {
+    console.log(data);
+    $("#notes").empty();
+  });
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
